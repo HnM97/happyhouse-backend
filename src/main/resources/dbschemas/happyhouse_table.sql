043a30c396633c877fcd1eba91aeb1f37f12fc26
chohm1223 <chohm1223@naver.com>
2022-11-23 09:35:23 +0900

    update sql

--- src/main/resources/dbschemas/happyhouse_table.sql
+++ src/main/resources/dbschemas/happyhouse_table.sql
@@ -14,21 +14,20 @@ USE happyhouse ;
 
 DROP TABLE IF EXISTS happyhouse.members ;
 CREATE TABLE IF NOT EXISTS happyhouse.members (
-  user_id VARCHAR(16) NOT NULL,
-  user_name VARCHAR(20) NOT NULL,
-  user_password VARCHAR(100) NOT NULL,
-  email_id VARCHAR(20) NULL DEFAULT NULL,
-  email_domain VARCHAR(45) NULL DEFAULT NULL,
-  join_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  token VARCHAR(1000) NULL DEFAULT NULL,
-  PRIMARY KEY (user_id))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    user_id VARCHAR(16) NOT NULL,
+    user_name VARCHAR(20) NOT NULL,
+    user_password VARCHAR(16) NOT NULL,
+    email_id VARCHAR(20) NULL DEFAULT NULL,
+    email_domain VARCHAR(45) NULL DEFAULT NULL,
+    join_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id))
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
-insert into happyhouse.members (user_id, user_name, user_password, email_id, email_domain, join_date, token)
-values     ('ssafy', '김싸피', '1234', 'ssafy', 'ssafy.com', now(), null), 
-        ('admin', '관리자', '1234', 'admin', 'google.com', now(), null);
+insert into happyhouse.members (user_id, user_name, user_password, email_id, email_domain, join_date)
+values     ('ssafy', '김싸피', '1234', 'ssafy', 'ssafy.com', now()),
+           ('admin', '관리자', '1234', 'admin', 'google.com', now());
 
 commit;
 
@@ -37,93 +36,93 @@ commit;
 DROP TABLE IF EXISTS happyhouse.board ;
 
 CREATE TABLE IF NOT EXISTS happyhouse.board (
-  article_no INT NOT NULL AUTO_INCREMENT,
-  user_id VARCHAR(16) NULL DEFAULT NULL,
-  subject VARCHAR(100) NULL DEFAULT NULL,
-  content VARCHAR(2000) NULL DEFAULT NULL,
-  hit INT NULL DEFAULT 0,
-  register_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (article_no),
-  INDEX board_to_members_user_id_fk (user_id ASC) VISIBLE,
-  CONSTRAINT board_to_members_user_id_fk
+                                                article_no INT NOT NULL AUTO_INCREMENT,
+                                                user_id VARCHAR(16) NULL DEFAULT NULL,
+    subject VARCHAR(100) NULL DEFAULT NULL,
+    content VARCHAR(2000) NULL DEFAULT NULL,
+    hit INT NULL DEFAULT 0,
+    register_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (article_no),
+    INDEX board_to_members_user_id_fk (user_id ASC) VISIBLE,
+    CONSTRAINT board_to_members_user_id_fk
     FOREIGN KEY (user_id)
     REFERENCES happyhouse.members (user_id))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 1
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 -- create table QnA
 
 CREATE TABLE IF NOT EXISTS happyhouse.qna (
-  user_id VARCHAR(16) NOT NULL,
-  article_no INT NOT NULL AUTO_INCREMENT,
-  subject VARCHAR(100) NULL DEFAULT NULL,
-  content VARCHAR(2000) NULL DEFAULT NULL,
-  hit INT NULL DEFAULT 0,
-  register_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (article_no, user_id),
-  INDEX qna_to_members_user_id_fk (user_id ASC) VISIBLE,
-  CONSTRAINT qna_to_members_user_id_fk
+    user_id VARCHAR(16) NOT NULL,
+    article_no INT NOT NULL AUTO_INCREMENT,
+    subject VARCHAR(100) NULL DEFAULT NULL,
+    content VARCHAR(2000) NULL DEFAULT NULL,
+    hit INT NULL DEFAULT 0,
+    register_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (article_no, user_id),
+    INDEX qna_to_members_user_id_fk (user_id ASC) VISIBLE,
+    CONSTRAINT qna_to_members_user_id_fk
     FOREIGN KEY (user_id)
     REFERENCES happyhouse.members (user_id))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 1
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 -- Table happyhousehouse.memo
 
 DROP TABLE IF EXISTS happyhouse.memo ;
 
 CREATE TABLE IF NOT EXISTS happyhouse.memo (
-  memo_no INT NOT NULL AUTO_INCREMENT,
-  user_id VARCHAR(16) NULL DEFAULT NULL,
-  comment VARCHAR(500) NULL DEFAULT NULL,
-  memo_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  article_no INT NULL DEFAULT NULL,
-  PRIMARY KEY (memo_no),
-  INDEX memo_to_qna_article_no_fk (article_no ASC) VISIBLE,
-  INDEX memo_toqnaqna_member_fk_idx (user_id ASC) VISIBLE,
-  CONSTRAINT memo_to_qna_article_no_fk
+                                               memo_no INT NOT NULL AUTO_INCREMENT,
+                                               user_id VARCHAR(16) NULL DEFAULT NULL,
+    comment VARCHAR(500) NULL DEFAULT NULL,
+    memo_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    article_no INT NULL DEFAULT NULL,
+    PRIMARY KEY (memo_no),
+    INDEX memo_to_qna_article_no_fk (article_no ASC) VISIBLE,
+    INDEX memo_toqnaqna_member_fk_idx (user_id ASC) VISIBLE,
+    CONSTRAINT memo_to_qna_article_no_fk
     FOREIGN KEY (article_no)
     REFERENCES happyhouse.qna (article_no)
     on delete cascade,
-  CONSTRAINT memo_to_member_user_id_fk
+    CONSTRAINT memo_to_member_user_id_fk
     FOREIGN KEY (user_id)
     REFERENCES happyhouse.members (user_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 1
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
-DROP TABLE IF EXISTS interest;
+drop table interest;
 CREATE TABLE IF NOT EXISTS happyhouse.interest (
-  user_id VARCHAR(16) NOT NULL,
-  dongcode VARCHAR(10) NOT NULL,
-  PRIMARY KEY (user_id, dongcode))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    user_id VARCHAR(16) NOT NULL,
+    dongcode VARCHAR(10) NOT NULL,
+    PRIMARY KEY (user_id, dongcode))
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 alter table houseinfo add hit int default 0;
 
-DROP TABLE IF EXISTS bookmark;
+drop table if exists bookmark;
 create table bookmark (
-	user_id varchar(16),
-    aptCode  bigint,
-    dongCode varchar(10),
-    primary key(user_id, aptCode),
-    foreign key(user_id) references `members`(`user_id`),
-    foreign key(aptCode) references `houseinfo`(`aptCode`)
+                          user_id varchar(16),
+                          aptCode  bigint,
+                          dongCode varchar(10),
+                          primary key(user_id, aptCode),
+                          foreign key(user_id) references `members`(`user_id`),
+                          foreign key(aptCode) references `houseinfo`(`aptCode`)
 );
 
 
@@ -141,26 +140,52 @@ select hi.aptCode, hi.dongcode, apartmentName, buildYear, dealAmount, area, deal
 from houseinfo hi, housedeal hd
 where hi.aptcode = hd.aptcode;
 
+create index houseinfo_idx on houseinfo(aptCode);
+create index housedeal_idx on houseinfo(aptCode);
+
+drop view apartment;
+create table apartment as
+select DISTINCT aptCode, dongcode, buildYear, apartmentName, roadName, jibun, lng, lat, min(cast(replace(dealAmount, ",","") as UNSIGNED)) as minAmount, max(cast(replace(dealAmount, ",","") as UNSIGNED)) as maxAmount
+from house
+group by aptCode;
+
+
 drop view apartment;
 create view apartment as
 select DISTINCT aptCode, dongcode, buildYear, apartmentName, roadName, jibun, lng, lat
-from house;
+from house
+group by aptCode;
 
+
+
+select * from house;
+desc apartment;
+
+select count(aptCode) from house;
+select count(aptCode) from apartment;
+
+select * from apartment limit 10;
+
+desc house;
+select * from house where aptCode = "11110000000006";
 
 drop view sido;
 create view sido as
 select left(dongCode,2) dongcode, sidoName
-from dongcode
-GROUP BY left(dongcode,2);
+        from dongcode
+        GROUP BY left(dongcode,2);
 
 drop view gugun;
 create view gugun as
 select left(dongCode,5) dongcode, gugunName
-from dongcode
-group by left(dongCode,5);
+        from dongcode
+        group by left(dongCode,5);
 
 drop view dong;
 create view dong as
 select left(dongCode,8) dongcode, dongName
-from dongcode
-group by left(dongCode,8);
+        from dongcode
+        group by left(dongCode,8);
+
+
+
